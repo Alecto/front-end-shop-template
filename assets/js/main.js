d1f011585038e0bacfd5863d46eddf4bfeba851f
Alecto <fomenkoandrey1978@gmail.com>
2022-06-11 11:40:18 +0300

    обновление кода

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ const incrementCounter = (label, cn) => {
   const counter = cn + 1;
 
   label.innerHTML = `${counter}`;
-  if (counter === 1) cartCounterLabel.style.display = 'block';
+  if (counter === 1) label.style.display = 'block';
 
   return counter;
 };
@@ -20,14 +20,14 @@ const getMockData = (t) => +t.parentElement
 
 const getPrice = (t, price) => Math.round((price + getMockData(t)) * 100) / 100;
 
-const disableControls = (t, fn) => {
-  contentContainer.removeEventListener('click', fn);
+const disableControls = (t, el, fn) => {
   t.disabled = true;
+  el.removeEventListener('click', fn);
 };
 
-const enableControls = (t, fn) => {
-  contentContainer.addEventListener('click', fn);
+const enableControls = (t, el, fn) => {
   t.disabled = false;
+  el.addEventListener('click', fn);
 };
 
 const btnClickHandler = (e) => {
@@ -45,17 +45,14 @@ const btnClickHandler = (e) => {
   if (target.matches('.item-actions__cart')) {
 
     cartCounter = incrementCounter(cartCounterLabel, cartCounter);
-
     cartPrice = getPrice(target, cartPrice);
     restoreHTML = target.innerHTML;
-
     target.innerHTML = `Added ${cartPrice.toFixed(2)} $`;
-
-    disableControls(target, btnClickHandler);
+    disableControls(target, contentContainer, btnClickHandler);
 
     setTimeout(() => {
       target.innerHTML = restoreHTML;
-      enableControls(target, btnClickHandler);
+      enableControls(target, contentContainer, btnClickHandler);
     }, interval);
   }
 };
