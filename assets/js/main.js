0b605ea05f9ce032b6bd43ba5ecbc271942b0891
Alecto <fomenkoandrey1978@gmail.com>
2022-12-26 12:27:29 +0200

    обновление кода

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,57 +4,53 @@ const cartCounterLabel = document.querySelector('#cart-counter-label');
 let cartCounter = 0;
 let cartPrice = 0;
 
-const incrementCounter = (label, cn) => {
-  const counter = cn + 1;
-
-  label.innerHTML = `${counter}`;
-  if (counter === 1) label.style.display = 'block';
-
-  return counter;
-};
-
-const getMockData = (t) => +t.parentElement
-  .previousElementSibling
-  .innerHTML
-  .replace(/^\$(\d+)\s\D+(\d+).*$/u, '$1.$2');
-
-const getPrice = (t, price) => Math.round((price + getMockData(t)) * 100) / 100;
-
-const disableControls = (t, el, fn) => {
-  t.disabled = true;
-  el.removeEventListener('click', fn);
-};
-
-const enableControls = (t, el, fn) => {
-  t.disabled = false;
-  el.addEventListener('click', fn);
-};
-
 const btnClickHandler = (e) => {
   const target = e.target;
   const interval = 2000;
 
   let restoreHTML = null;
 
-  if (typeof target !== 'object') {
-    console.error('target не является объектом.');
+  if (typeof target !== 'object') return;
+  if (!target.matches('.item-actions__cart')) return;
 
-    return;
-  }
+  incrementCounter(cartCounterLabel, ++cartCounter);
+  cartPrice = getPrice(target, cartPrice, getMockData);
 
-  if (target.matches('.item-actions__cart')) {
+  restoreHTML = target.innerHTML;
+  target.innerHTML = `Added ${cartPrice.toFixed(2)} $`;
 
-    cartCounter = incrementCounter(cartCounterLabel, cartCounter);
-    cartPrice = getPrice(target, cartPrice);
-    restoreHTML = target.innerHTML;
-    target.innerHTML = `Added ${cartPrice.toFixed(2)} $`;
-    disableControls(target, contentContainer, btnClickHandler);
+  disableControls(target, contentContainer, btnClickHandler);
 
-    setTimeout(() => {
-      target.innerHTML = restoreHTML;
-      enableControls(target, contentContainer, btnClickHandler);
-    }, interval);
-  }
+  setTimeout(() => {
+    target.innerHTML = restoreHTML;
+    enableControls(target, contentContainer, btnClickHandler);
+  }, interval);
 };
 
 contentContainer.addEventListener('click', btnClickHandler);
+
+function incrementCounter($label, cn) {
+  $label.innerHTML = `${cn}`;
+  if (cn === 1) $label.style.display = 'block';
+}
+
+function getMockData(t) {
+  return +t.parentElement
+      .previousElementSibling
+      .innerHTML
+      .replace(/^\$(\d+)\s\D+(\d+).*$/, '$1.$2');
+}
+
+function getPrice(t, p, cb) {
+  return Math.round((p + cb(t)) * 100) / 100;
+}
+
+function disableControls(t, el, fn) {
+  t.disabled = true;
+  el.removeEventListener('click', fn);
+}
+
+function enableControls(t, el, fn) {
+  t.disabled = false;
+  el.addEventListener('click', fn);
+}
