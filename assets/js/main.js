7a22ae0ecc6e0bd7f7bdb6311b8dbdce7cc69cfd
Alecto <fomenkoandrey1978@gmail.com>
2024-05-08 20:21:26 +0300

    обновление кода

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,18 +5,15 @@ let cartCounter = 0;
 let cartPrice = 0;
 
 const btnClickHandler = (e) => {
-  const target = e.target;
+  const {target} = e;
   const interval = 2000;
 
-  let restoreHTML = null;
+  if (!target?.matches('button.item-actions__cart')) return
 
-  if (typeof target !== 'object') return;
-  if (!target.matches('.item-actions__cart')) return;
+  renderCartCounterLabel(cartCounterLabel, ++cartCounter)
+  cartPrice = getPrice(target, cartPrice)
 
-  incrementCounter(cartCounterLabel, ++cartCounter);
-  cartPrice = getPrice(target, cartPrice);
-
-  restoreHTML = target.innerHTML;
+  let restoreHTML = target.innerHTML;
   target.innerHTML = `Added ${cartPrice.toFixed(2)} $`;
 
   disableControls(target, contentContainer, btnClickHandler);
@@ -25,16 +22,17 @@ const btnClickHandler = (e) => {
     enableControls(target, contentContainer, btnClickHandler);
     target.innerHTML = restoreHTML;
   }, interval);
-};
+}
 
 contentContainer.addEventListener('click', btnClickHandler);
 
-function incrementCounter($label, counter) {
-  $label.innerHTML = `${counter}`;
-  if (counter === 1) $label.style.display = 'block';
+function renderCartCounterLabel(label, counter) {
+  label.textContent = `${counter}`;
+  if (counter === 1)
+    label.style.display = 'block';
 }
 
-function getMockData(target) {
+function getMockPrice(target) {
   return +target.parentElement
       .previousElementSibling
       .innerHTML
@@ -42,15 +40,15 @@ function getMockData(target) {
 }
 
 function getPrice(target, price) {
-  return Math.round((price + getMockData(target)) * 100) / 100;
+  return Math.round((price + getMockPrice(target)) * 100) / 100;
 }
 
-function disableControls(target, $container, handler) {
+function disableControls(target, container, handler) {
   target.disabled = true;
-  $container.removeEventListener('click', handler);
+  container.removeEventListener('click', handler);
 }
 
-function enableControls(target, $container, handler) {
+function enableControls(target, container, handler) {
   target.disabled = false;
-  $container.addEventListener('click', handler);
+  container.addEventListener('click', handler);
 }
